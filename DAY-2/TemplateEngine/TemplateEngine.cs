b532a14bdad94bd03520fe449e4274d70c255d75
Alekhya Dasari <alekhyadasari.1603@gmail.com>
2025-03-13 16:23:41 +0530

    Create TemplateEngine.cs

---
+++ DAY-2/TemplateEngine/TemplateEngine.cs
@@ -0,0 +1,28 @@
+namespace TemplateEngine
+{
+    public class TemplateEngine
+    {
+        private string template;
+        private Dictionary<string, string> variables = new Dictionary<string, string>();
+
+        public string Evaluate()
+        {
+            string result = template;
+            foreach (var variable in variables)
+            {
+                result = result.Replace("{" + variable.Key + "}", variable.Value);
+            }
+            return result;
+        }
+
+        public void SetTemplate(string template)
+        {
+            this.template = template;
+        }
+
+        public void SetVariable(string name, string value)
+        {
+            variables[name] = value;
+        }
+    }
+}
